4d4e28efaad76c761f39bd42a2e5363a2ac372e5
Alifian Alfirazi <alifian.alfirazi@dansmultipro.com>
2026-08-10 20:59:08 +0700

    create shell and layouts

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app/App";
+import { queryClient } from "./app/queryClient";
+import "./app/styles/index.css";
+
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>,
+);
